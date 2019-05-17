52e3246aa37594b6b5331a3b4f166cfd78dfd462
HTrayford <bowman_trayford@hotmail.com>
2019-05-17 09:39:39 +0300

    Fixed a bug where CanvasContext Font Size would be set to GIANT during demo and not returned to small size on return to Title Screen.

--- js/GraphicsCommon.js
+++ js/GraphicsCommon.js
@@ -1,13 +1,17 @@
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
+	canvasContext.save();
 	canvasContext.fillStyle = fillColor;
 	canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+	canvasContext.restore();
 }
 
 function colorCircle(centerX, centerY, radius, fillColor) {
+	canvasContext.save();
 	canvasContext.fillStyle = 'white';
 	canvasContext.beginPath();
 	canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
 	canvasContext.fill();
+	canvasContext.restore();
 }
 
 function drawBitMap(graphic, x, y) {
@@ -26,22 +30,28 @@ function drawBitmapCenteredWithRotation(useBitmap, atX, atY, withAng) {
   }
 
 function colorText(showWords, textX, textY, fillColor, font = "14px Arial Black") {
+	canvasContext.save();
   canvasContext.textAlign = "left";
   canvasContext.fillStyle = fillColor;
   canvasContext.font = font;
-  canvasContext.fillText(showWords, textX, textY);
+	canvasContext.fillText(showWords, textX, textY);
+	canvasContext.restore();
 }
 
 function colorTextCentered(showWords, textX, textY, fillColor, font = "14px Arial Black") {
+	canvasContext.save();
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = fillColor;
   canvasContext.font = font;
-  canvasContext.fillText(showWords, textX, textY);
+	canvasContext.fillText(showWords, textX, textY);
+	canvasContext.restore();
 }
 
 function largerColorText(showWords, textX, textY, fillColor, font = "56px Arial Black") {
+	canvasContext.save();
   canvasContext.textAlign = "left";
   canvasContext.fillStyle = fillColor;
   canvasContext.font = font;
-  canvasContext.fillText(showWords, textX, textY);
+	canvasContext.fillText(showWords, textX, textY);
+	canvasContext.restore();
 }
