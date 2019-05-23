2d191291e535902e40ff00b3600bb6e41d0a06f5
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2019-05-22 22:08:04 -0300

    Undo the test condition on Pill.js(ENABLED_PILLS = [magnetPill])

--- js/Pill.js
+++ js/Pill.js
@@ -6,12 +6,12 @@ const STRETCHED_PADDLE_MULTIPLIER = 2;
 const SHRINK_PADDLE_MULTIPLIER = 0.5;
 const MULTI_BALL_QUANTITY = 2;
 const PILL_DROP_CHANCE = 0.4;
-//const ENABLED_PILLS = [pointsPill, stretchPill, ghostPill, multiBallPill, stickyBallPill, shrinkPill, accellPill, moveUpPill, invaderPill, jumpPill, extraLifePill, letterGPill, letterAPill, letterMPill, letterKPill, letterEPill, letterDPill, letterOPill];
+const ENABLED_PILLS = [pointsPill, stretchPill, ghostPill, multiBallPill, stickyBallPill, shrinkPill, accellPill, moveUpPill, invaderPill, jumpPill, extraLifePill, letterGPill, letterAPill, letterMPill, letterKPill, letterEPill, letterDPill, letterOPill];
 
 // used for testing specific powerups - comment out other initializations
 //const PILL_DROP_CHANCE = 1.1; //Math.random is 0-1 so random will always be < 1.1;
 //const ENABLED_PILLS = [multiBallPill]; 
-const ENABLED_PILLS = [magnetPill]; 
+//const ENABLED_PILLS = [magnetPill]; 
 
 var pills = [];
 
