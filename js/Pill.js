74b6717aea8713cd08b33ff1be659146ce0884e5
vmckeown <35244445+vmckeown@users.noreply.github.com>
2019-06-15 13:51:53 -0400

    WIP:  Hat Pill implemented.  Power needs to be written.

--- js/Pill.js
+++ js/Pill.js
@@ -12,7 +12,7 @@ const SHRINK_PADDLE_MULTIPLIER = 0.5;
 const MULTI_BALL_QUANTITY = 2;
 const PILL_DROP_CHANCE = 0.4;
 
-var ENABLED_PILLS = [pointsPill, stretchPill, ghostPill, multiBallPill, stickyBallPill, shrinkPill, accellPill, moveUpPill, invaderPill, jumpPill, gunPill, extraLifePill, letterGPill, letterAPill, letterMPill, letterKPill, letterEPill, letterDPill, letterOPill];
+var ENABLED_PILLS = [pointsPill, stretchPill, ghostPill, multiBallPill, stickyBallPill, shrinkPill, accellPill, moveUpPill, invaderPill, jumpPill, gunPill, extraLifePill, letterGPill, letterAPill, letterMPill, letterKPill, letterEPill, letterDPill, letterOPill, hatPill];
 
 // used for testing specific powerups - comment out other initializations
 //const PILL_DROP_CHANCE = 1.1; //Math.random is 0-1 so random will always be < 1.1;
@@ -352,6 +352,21 @@ function letterOPill() {
 		this.sound.play();
 	}
 	checkForGAMKEDO();
+}
+
+hatPill.prototype = new pillClass();
+function hatPill() {
+	this.name = "hatPill";
+	this.imageOffsetX = 0;
+	this.imageOffsetY = PILL_H * 10;
+	this.powerTime = framesPerSecond/2 * 10;
+	this.startPower = function () {
+		//
+	}
+
+	this.endPower = function () {
+		//
+	}
 }
 
 function checkForGAMKEDO(){
